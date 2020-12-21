85a4e0b86010774e0ec54c6b25ae93eab1ad1d7e
namhihi237 <yentth237@gmail.com>
2020-12-21 21:19:08 +0700

    search ...

--- backend/configs/env.js
+++ backend/configs/env.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
 export const envVariables = {
-    port: process.env.PORT || 80,
+    port: process.env.PORT || 5000,
 
-    // mongoURI:
-    //     process.env.DB_URI ||
-    //     "mongodb+srv://ttcn13:thuctapcongnhan@cluster0.0orjn.mongodb.net/thuctapcn?retryWrites=true&w=majority",
-    mongoURI: process.env.DB_URI || "mongodb://localhost:27017/ttcn",
+    mongoURI:
+        process.env.DB_URI ||
+        "mongodb+srv://ttcn13:thuctapcongnhan@cluster0.0orjn.mongodb.net/thuctapcn?retryWrites=true&w=majority",
+    // mongoURI: process.env.DB_URI || "mongodb://localhost:27017/ttcn",
 
     jwtSecret: "alkndaksdaskdna!@",
     nodeEnv: process.env.NODE_ENV || "development",
