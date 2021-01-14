8572cc8a99e71d4a26ca661d2f02d0fcd2c888aa
namhihi237 <yentth237@gmail.com>
2021-01-14 15:41:35 +0700

    xxx

--- backend/configs/env.js
+++ backend/configs/env.js
@@ -2,18 +2,15 @@ require("dotenv").config();
 
 export const envVariables = {
     port: process.env.PORT || 5000,
-    mongoURI:
-        process.env.DB_URI ||
-        "mongodb+srv://ttcn13:thuctapcongnhan@cluster0.0orjn.mongodb.net/thuctapcn?retryWrites=true&w=majority",
-    // mongoURI: process.env.DB_URI || "mongodb://localhost:27017/ttcn",
+    mongoURI: process.env.DB_URI || "mongodb://localhost:27017/ttcn",
     jwtSecret: "alkndaksdaskdna!@",
     nodeEnv: process.env.NODE_ENV || "development",
-    gmail: process.env.GMAIL || "noreply.strangerfunny@gmail.com",
-    pass: process.env.PASS || "kaydateamqQ!@#",
+    gmail: process.env.GMAIL,
+    pass: process.env.PASS,
     text: process.env.TEXT || "Code reset password: ",
     subject: process.env.SUBJECT || "[CODE RESET :]",
-    cloudName: "dut-t1-99",
-    api_key_cloud: "524472935912584",
-    api_secret_cloud: "CD1P6czcQ-wUQdwbYDOXn8BmUjE",
+    cloudName: process.env.CLOUD_NAME,
+    api_key_cloud: process.env.API_KEY_CLOUD,
+    api_secret_cloud: process.env.API_SECRET_CLOUD,
     key_admin: "123qwe!@#",
 };
